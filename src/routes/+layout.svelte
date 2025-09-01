068183cf51704ecb8fc907ece97c25e9416af2c7
Baconway <155370111+Baconway@users.noreply.github.com>
2025-09-01 11:22:41 +0700

    layout changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 	<a class="p-2 bg-gray-700 text-white rounded-xs" href="/portfolio">Portfolio</a>
 </div>
 <p class="inline ">pretend this is a logo</p>
-<img class="w-[1.75em] pr-1 hover:cursor-pointer" src="../src/routes/themeSwitch.svg" alt="theme switching button">
+<img class="w-[1.75em] pr-1 hover:cursor-pointer pl-0.5" src="../src/routes/themeSwitch.svg" alt="theme switching button">
 </nav>
 
 {@render children()}
