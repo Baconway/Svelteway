b4631e47c6aeb3d9b68bc8bbe96868cbb11c5eb6
Baconway <Baconway@users.noreply.github.com>
2025-09-22 17:37:57 +0700

    Navbar modifications

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,30 @@
 <script>
 	let { children } = $props();
 	import "../applet.css";
+  import Page from "./+page.svelte";
+
+	function themeChange(theme) {
+		console.log(document.documentElement.className )
+		if (document.documentElement.className == 'dark') {
+			document.documentElement.className = 'light'
+		} else {
+			document.documentElement.className = 'dark'
+		}
+	}
 </script>
 
-<nav class="flex border-b-5 text-[20px] p-1 sticky justify-between items-center">
+<nav class="flex border-b-4 border-amber-50 text-[20px] p-1 sticky justify-between items-center bg-gray-500">
 	<div class="flex gap-1 ">
-	<a class="p-2 bg-gray-700 text-white rounded-xs" href="/">Home</a>
-	<a class="p-2 bg-gray-700 text-white rounded-xs" href="/portfolio">Portfolio</a>
+	<a class="p-2 dark:text-white text-black rounded-xs hover:bg-blue-800" href="/">Home</a>
+	<a class="p-2 dark:text-white text-black rounded-xs hover:bg-blue-800" href="/portfolio">Portfolio</a>
 </div>
-<p class="inline ">pretend this is a logo</p>
-<img class="w-[1.75em] pr-1 hover:cursor-pointer pl-0.5" src="../src/routes/themeSwitch.svg" alt="theme switching button">
+<p class="inline dark:text-white text-black" >pretend this is a logo</p>
+
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+
+<img class="w-[1.75em] pr-1 hover:cursor-pointer pl-0.5 dark:invert" onclick={() => themeChange()} id="switch-theme" src="../src/routes/themeSwitch.svg" alt="theme switching button">
+
 </nav>
 
 {@render children()}
