ecb175f9af981da0395dace1b1b19ad5623fbc79
Baconway <Baconway@users.noreply.github.com>
2025-10-09 19:20:38 +0700

    Reworked Info Bar

--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -3,39 +3,14 @@
 </script>
 
 <div class="flex flex-row items-center justify-center gap-4 w-[64%] mt-1">
-  <a href={links[0].url}
-    ><img
-      class="w-10 h-10 dark:invert"
-      alt="img 1"
-      src={links[0].path}
-      title={links[0].title}
-    /></a
-  >
-
-  <a href={links[1].url}
-    ><img
-      class="w-10 h-10 not-dark:invert"
-      alt="img 2"
-      src={links[1].path}
-      title={links[1].title}
-    /></a
-  >
-
-  <a href={links[2].url}
-    ><img
-      class="w-10 h-10 dark:invert"
-      alt="img 1"
-      src={links[2].path}
-      title={links[2].title}
-    /></a
-  >
-
-  <a href={links[3].url}
-    ><img
-      class="w-10 h-10 dark:invert"
-      alt="img 3"
-      src={links[3].path}
-      title={links[3].title}
-    /></a
-  >
+  {#each links as media}
+    <a href={media.url}
+      ><img
+        class="w-10 h-10 dark:invert"
+        alt={media.alt}
+        src={media.path}
+        title={media.title}
+      /></a
+    >
+  {/each}
 </div>
