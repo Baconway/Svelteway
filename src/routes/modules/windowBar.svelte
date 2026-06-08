3070b2f552f14538a55422c83f1693f73abfe865
Baconway <Baconway@users.noreply.github.com>
2026-06-08 16:18:58 +0700

    Updated Window Bar

--- src/routes/modules/windowBar.svelte
+++ src/routes/modules/windowBar.svelte
@@ -16,10 +16,7 @@
   style="background-color: {data.palette[0]};"
   class="sticky top-0 z-10 flex flex-row w-full pb-1 px-1 bg-linear-to-br justify-between items-center"
 >
-  <div class="flex flex-row items-center justify-center align-middle pl-2">
-    <a href={resolveRoute("/")}
-      ><BackIcon width="24px" height="24px" color="black" /></a
-    >
+  <div class="flex flex-row items-center justify-center align-middle pl-1">
     <img class="w-7 h-7" src="/shiroko.gif" alt="icon" draggable="false" />
     <p style="color: {shouldTextBeBlack(data.palette[0]) ? 'black' : 'white'}">
       Information
@@ -27,13 +24,10 @@
   </div>
 
   <div class="flex">
-    <button
-      class="px-1 border-t-0 border-2 border-shiroko-5 opacity-10 rounded-bl-md"
-      ><Window_min width="24px" height="24px" /></button
-    >
-    <button
-      class="px-1 border-l-0 border-t-0 border-2 border-shiroko-5 opacity-10"
-      ><Window_max width="24px" height="24px" /></button
+    <a
+      class="px-3 border-l-0 border-t-0 border-2 border-salt-3 rounded-bl-md bg-shiroko-3"
+      href={resolveRoute("/")}
+      ><BackIcon width="24px" height="24px" color="black" /></a
     >
     <button
       class="px-3 border-b-2 border-b-salt-3 rounded-br-md bg-red-400"
